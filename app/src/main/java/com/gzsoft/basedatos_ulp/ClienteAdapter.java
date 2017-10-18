5e8e58f673f3b55c59149114803207d88ba6fc2b
LcsGrz <lcsjuegos@gmail.com>
2017-10-18 12:41:18 -0300

    Un poco mas...

---
+++ app/src/main/java/com/gzsoft/basedatos_ulp/ClienteAdapter.java
@@ -0,0 +1,35 @@
+package com.gzsoft.basedatos_ulp;
+
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.provider.BaseColumns;
+
+public class ClienteAdapter {
+    private static final String NAME = "cliente";
+    private SQLiteDatabase sqlDB;
+
+    public ClienteAdapter(SQLiteDatabase sqlDB){
+        this.sqlDB = sqlDB;
+    }
+
+    private class Colums implements BaseColumns{
+        public final static String ID = "cliente_id";
+        public final static String NOMBRE = "nombre";
+        public final static String APELLIDO = "apellido";
+        public final static String TELEFONO = "telefono";
+    }
+
+    private final static String[] COLUMS ={
+            Colums.ID,Colums.NOMBRE,Colums.APELLIDO,Colums.TELEFONO
+    };
+
+    public final static String CR_TABLE = "create table if no exists" + NAME + "(" + Colums.ID + "integer primary key autoincrement," + Colums.NOMBRE + "text not nutll," + Colums.APELLIDO + "text not null," + Colums.TELEFONO + "text);";
+
+public boolean insert (String nombre, String apellido, String telefono){
+    ContentValues values = new ContentValues();
+    values.put(Colums.NOMBRE, nombre);
+    values.put(Colums.APELLIDO, apellido);
+    values.put(Colums.TELEFONO, telefono);
+    return sqlDB.insert(NAME,null,values) > 0;
+}
+}
